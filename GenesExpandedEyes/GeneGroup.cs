e25a24a6db025c9aaff7234ebdb95c12167ce9eb
Jake <Strayajake@Outlook.com>
2023-01-24 11:55:20 +0800

    Script and genes

---
+++ GenesExpandedEyes/GeneGroup.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace LFS_GenesExpandedEyes
+{
+    public class GeneGroup : Def
+    {
+        public List<GeneDef> genes;
+    }
+
+    public class GeneGroups : Def
+    {
+        public List<GeneGroup> geneGroups;
+    }
+}
---
+++ GenesExpandedEyes/GenesExpandedGeneSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace LFS_GenesExpandedEyes
+{
+    [StaticConstructorOnStartup]
+    public static class GeneExpandedGeneSpawner
+    {
+        private static readonly Type patchType = typeof(GeneExpandedGeneSpawner);
+
+        static GeneExpandedGeneSpawner()
+        {
+            Harmony _harmony = new Harmony("GeneExpandedGeneSpawner");
+            _harmony.Patch(
+                AccessTools.Method(typeof(PawnGenerator),
+                    typeof(PawnGenerator).GetMethod("GenerateGenes", BindingFlags.Static | BindingFlags.NonPublic).Name,
+                    null, null), null, new HarmonyMethod(GeneExpandedGeneSpawner.patchType, "PostFixGenes", null),
+                null, null);
+        }
+
+        private static void PostFixGenes(Pawn pawn, XenotypeDef xenotypeDef, PawnGenerationRequest request)
+        {
+            var isBaby = request.AllowedDevelopmentalStages == DevelopmentalStage.Newborn;
+
+            if (!isBaby)
+            {
+                var customXeno = pawn.genes.UniqueXenotype;
+                if (!customXeno)
+                {
+                    GeneGroups possibleEndotypes =
+                        DefDatabase<GeneGroups>.AllDefs.FirstOrDefault((GeneGroups x) =>
+                            string.Equals(x.defName, pawn.genes.Xenotype.defName,
+                                StringComparison.CurrentCultureIgnoreCase));
+
+                    bool doesHaveEndoType = possibleEndotypes != null;
+                    if (doesHaveEndoType)
+                    {
+                        foreach (var geneGroup in possibleEndotypes.geneGroups)
+                        {
+                            var rnd = new Random();
+                            pawn.genes.AddGene(geneGroup.genes[rnd.Next(0, geneGroup.genes.Count)], false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
