10a106ad4eb787616059793d44e0893a6e084630
Jake <Strayajake@Outlook.com>
2023-01-24 17:52:01 +0800

    Assembly and xeno expanded support

---
+++ LFSGeneExpandedEyes/GeneGroup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Verse;
+
+namespace LFS_GenesExpandedEyes
+{
+    public class GeneChances
+    {
+        public GeneDef geneDef;
+        
+        public GeneChances(GeneDef geneDef, float chance)
+        {
+            this.geneDef = geneDef;
+        }
+        
+        public GeneChances()
+        {
+        }
+        
+        public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+        {
+            DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "geneDef", xmlRoot.Name, null, null, null);
+        }
+    }
+    
+    public class GeneGroup
+    {
+        public GeneGroup()
+        {
+        }
+        
+        public List<GeneChances> Endogenes = new List<GeneChances>();
+        public GeneGroup(List<GeneChances> endogeneChances)
+        {
+            this.Endogenes = endogeneChances;
+        }
+    }
+
+    public class GeneGroups : Def
+    {
+        public List<GeneGroup> geneGroups = new List<GeneGroup>();
+    }
+}
---
+++ LFSGeneExpandedEyes/GenesExpandedGeneSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace LFS_GenesExpandedEyes
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatch
+    {
+        private static readonly Type patchType = typeof(HarmonyPatch);
+
+        static HarmonyPatch()
+        {
+            Harmony harmony = new Harmony("GeneExpandedGeneSpawner");
+            harmony.Patch(AccessTools.Method(typeof(PawnGenerator), typeof(PawnGenerator).GetMethod(
+                        "GenerateGenes",
+                        BindingFlags.Static | BindingFlags.NonPublic).Name,
+                    null,
+                    null),
+                null,
+                new HarmonyMethod(HarmonyPatch.patchType, "PostfixGenerator", null), null, null);
+            Log.Message("[GeneExpandedEyes] harmony patch succeeded.");
+        }
+
+        public static void PostfixGenerator(Pawn pawn, XenotypeDef xenotype, PawnGenerationRequest request)
+        {
+            var isBaby = request.AllowedDevelopmentalStages == DevelopmentalStage.Newborn;
+
+            if (isBaby) return;
+            var customXeno = pawn.genes.UniqueXenotype;
+            
+            if (customXeno) return;
+            GeneGroups possibleEndotypes =
+                DefDatabase<GeneGroups>.AllDefs.FirstOrDefault((GeneGroups x) =>
+                    string.Equals(x.defName, pawn.genes.Xenotype.defName,
+                        StringComparison.CurrentCultureIgnoreCase));
+
+            var doesHaveEndoType = possibleEndotypes != null;
+            if (!doesHaveEndoType) return;
+            
+            var rnd = new Random();
+            foreach (var geneGroup in possibleEndotypes.geneGroups)
+            {
+                var randomNum = rnd.Next(0, geneGroup.Endogenes.Count);
+                pawn.genes.AddGene(geneGroup.Endogenes[randomNum].geneDef,
+                    false);
+            }
+        }
+    }
+}
